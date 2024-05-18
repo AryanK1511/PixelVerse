df07795bd9190ec5019d977c4c355bff6630f56d
Shrey Bhatt <shreybhatt130@yahoo.com>
2024-05-18 16:19:06 -0400

    half of the changes for /home showcase

--- hawk-fe/components/FullBentoGrid.tsx
+++ hawk-fe/components/FullBentoGrid.tsx
@@ -19,6 +19,7 @@ const style = {
   p: 4,
 };
 export function BentoGridDemo() {
+  const [projects, setProjects] = React.useState([] as any[]);
   const [gemError, setGemError] = React.useState("");
   const [item, setItem] = React.useState<{
     title: string;
@@ -78,14 +79,14 @@ export function BentoGridDemo() {
   return (
     <>
     <BentoGrid className="max-w-4xl mx-auto mt-24">
-      {items.map((item, i) => (
+      {projects.map((project, i) => (
         <BentoGridItem
           onClick={() => handleOpen(item)}
           key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          icon={item.icon}
+          title={project.title}
+          description={project.description}
+          header={<Skeleton img_link={project.sampleImages[0]}/>}
+          icon={<span>{project.pointsPerImage} P$</span>}
           className={i === 3 || i === 6 ? "md:col-span-2" : ""}
         />
       ))}
@@ -124,9 +125,9 @@ export function BentoGridDemo() {
     </>
   );
 }
-const Skeleton = () => (
+const Skeleton = ({img_link}) => (
   <img
-    src="demo1.png"
+    src={img_link}
     className="object-cover flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"
   ></img>
 );
@@ -134,44 +135,44 @@ const items = [
   {
     title: "The Dawn of Innovation",
     description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
+    header: <Skeleton img_link="demo1.png"/>,
     icon: <span>100 P$</span>,
   },
   {
     title: "The Digital Revolution",
     description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
+    header: <Skeleton img_link="demo1.png"/>,
     icon: <span>100 P$</span>,
   },
   {
     title: "The Art of Design",
     description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
+    header: <Skeleton img_link="demo1.png"/>,
     icon: <span>100 P$</span>,
   },
   {
     title: "The Power of Communication",
     description:
       "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
+    header: <Skeleton img_link="demo1.png"/>,
     icon: <span>100 P$</span>,
   },
   {
     title: "The Pursuit of Knowledge",
     description: "Join the quest for understanding and enlightenment.",
-    header: <Skeleton />,
+    header: <Skeleton img_link="demo1.png"/>,
     icon: <span>100 P$</span>,
   },
   {
     title: "The Joy of Creation",
     description: "Experience the thrill of bringing ideas to life.",
-    header: <Skeleton />,
+    header: <Skeleton img_link="demo1.png"/>,
     icon: <span>100 P$</span>,
   },
   {
     title: "The Spirit of Adventure",
     description: "Embark on exciting journeys and thrilling discoveries.",
-    header: <Skeleton />,
+    header: <Skeleton img_link="demo1.png"/>,
     icon: <span>100 P$</span>,
   },
 ];
