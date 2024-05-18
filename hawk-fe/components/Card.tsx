978fcf25f59348b2169e870c11ce79b85401f510
Waheeb Barqawi <trolltrolltrolllollollol@outlook.com>
2024-05-18 00:30:48 -0400

    added card component and made everything client-side

---
+++ hawk-fe/components/Card.tsx
@@ -0,0 +1,53 @@
+"use-client";
+
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "./3d-card";
+import Link from "next/link";
+
+interface Props {
+  title: string;
+  description: string;
+  imageLink: string;
+  points: number;
+}
+
+export function Card({ title, description, imageLink, points }: Props) {
+  return (
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          {title}
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          {description}
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src={imageLink}
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="mt-5">
+          <CardItem
+            as="p"
+            translateZ="60"
+            className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+          >
+            Points: {points}
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+  );
+}
