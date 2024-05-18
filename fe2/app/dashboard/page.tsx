d2931539391199f737486a98efa57b911508e818
Shrey Bhatt <shreybhatt130@yahoo.com>
2024-05-18 19:06:00 -0400

    dashboard done

--- fe2/app/dashboard/page.tsx
+++ fe2/app/dashboard/page.tsx
@@ -1,10 +1,21 @@
 "use client";
 import { Navbar } from "../components/Navbar";
+import DashboardInfo from "../components/DashboardInfo";
 
 export default function Dashboard() {
   return (
     <>
-      <Navbar />  
+      <Navbar /> 
+      <div className="container mx-auto p-4">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-1 mt-32">
+            <DashboardInfo />
+          </div>
+          <div className="col-span-2 mt-32">
+            Column 2
+          </div>
+        </div>
+      </div>
     </>
   );
 }
